7bc9504c514bfe815a089a542e1348b466e548ef
Grimberg Cryzan <gcryzan@gmail.com>
2017-05-22 21:23:46 -0300

    testando commit

---
+++ views/pagina-inicial.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>To Presente</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+    <!-- Personal css file -->
+    <link rel="stylesheet" href="css/style.css" />
+    
+</head>
+<body>
+    
+    <!-- Corpo do Site -->
+    <div class="container-fluid"">
+        <div class="row"> 
+            <div class="col-md-10 col-md-offset-1" style="border: 1 solid; background-color: black">
+                <center> 
+                    <h2 style="color: white">Stupid Nigger</h2>
+                </center>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
